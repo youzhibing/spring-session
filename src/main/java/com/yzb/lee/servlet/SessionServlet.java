47bf492a93b705f390daaef0686ac25075606efd
youzhibing <997914490@qq.com>
2017-08-11 14:56:37 +0800

    first commit

---
+++ src/main/java/com/yzb/lee/servlet/SessionServlet.java
@@ -0,0 +1,26 @@
+package com.yzb.lee.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class SessionServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		String attributeName = req.getParameter("attributeName");
+		String attributeValue = req.getParameter("attributeValue");
+		req.getSession().setAttribute(attributeName, attributeValue);
+		
+		resp.sendRedirect(req.getContextPath() + "/");
+	}
+	
+	
+
+}
