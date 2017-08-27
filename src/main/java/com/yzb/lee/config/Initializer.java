66f63efb0fb8907076bcf66d4f9fa29b7d76e7e6
youzhibing <997914490@qq.com>
2017-08-27 08:56:02 +0800

    注解实现spring-session的配置

---
+++ src/main/java/com/yzb/lee/config/Initializer.java
@@ -0,0 +1,12 @@
+package com.yzb.lee.config;
+
+import org.springframework.session.web.context.AbstractHttpSessionApplicationInitializer;
+
+public class Initializer extends AbstractHttpSessionApplicationInitializer
+{
+
+	public Initializer()
+	{
+		//super(Config.class);
+	}
+}
