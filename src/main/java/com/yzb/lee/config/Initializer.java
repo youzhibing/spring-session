a44a65d1e39415e1e4b9ca50d25e0f3ac7ff6d4a
youzhibing <997914490@qq.com>
2017-08-27 09:09:01 +0800

    xml、注解方式只能选择一种，用了xml(web.xml中的springSessionRepositoryFilter),注解方式(Initalizer、Config)就不需要了,不然spring容器会报重复初始化的错误

--- src/main/java/com/yzb/lee/config/Initializer.java
+++ src/main/java/com/yzb/lee/config/Initializer.java
@@ -2,11 +2,11 @@ package com.yzb.lee.config;
 
 import org.springframework.session.web.context.AbstractHttpSessionApplicationInitializer;
 
-public class Initializer extends AbstractHttpSessionApplicationInitializer
+/*public class Initializer extends AbstractHttpSessionApplicationInitializer
 {
 
 	public Initializer()
 	{
-		//super(Config.class);
+		super(Config.class);
 	}
-}
+}*/
